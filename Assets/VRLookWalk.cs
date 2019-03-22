43c1de8915c795c6e6b5660e306b6a4a7041512b
Aleksandar Petrushevski <petrushevskiapps@gmail.com>
2019-03-22 20:04:22 +0100

    VR Domasno 1

---
+++ Assets/VRLookWalk.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour {
+
+    [SerializeField]
+    Transform vrCamera;
+
+    [SerializeField]
+    float speed = 30f;
+
+    CharacterController character;
+	// Use this for initialization
+	void Start ()
+    {
+        character = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (vrCamera.eulerAngles.x >= -10f && vrCamera.eulerAngles.x < 10f)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            character.SimpleMove(forward * speed * Time.deltaTime);
+        }
+	}
+}
